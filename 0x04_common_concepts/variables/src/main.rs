1fb8aceed67bef02e7618b7ed9da58c62283f089
Cleve-codes <clevemomanyi@gmail.com>
2025-04-12 22:29:10 +0300

    Rust -vars

---
+++ 0x04_common_concepts/variables/src/main.rs
@@ -0,0 +1,17 @@
+
+// Immutable
+// fn main() {
+//     let x = 5;
+//     println!("The value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {x}");
+// }
+
+
+// Mutable
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
---
+++ 0x04_common_concepts/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/cleve/Documents/Rust-101/0x04_common_concepts/variables/target/debug/variables: /home/cleve/Documents/Rust-101/0x04_common_concepts/variables/src/main.rs
