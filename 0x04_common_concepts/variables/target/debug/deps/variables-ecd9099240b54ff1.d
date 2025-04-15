e1beb3dabaac41105d15efe7647e3093c58148a7
Cleve-codes <clevemomanyi@gmail.com>
2025-04-15 21:20:59 +0300

    rustlings- vars

--- 0x04_common_concepts/variables/target/debug/deps/variables-ecd9099240b54ff1.d
+++
@@ -1,5 +0,0 @@
-/home/cleve/Documents/Rust-101/0x04_common_concepts/variables/target/debug/deps/variables-ecd9099240b54ff1: src/main.rs
-
-/home/cleve/Documents/Rust-101/0x04_common_concepts/variables/target/debug/deps/variables-ecd9099240b54ff1.d: src/main.rs
-
-src/main.rs:
--- 0x04_common_concepts/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/home/cleve/Documents/Rust-101/0x04_common_concepts/variables/target/debug/variables: /home/cleve/Documents/Rust-101/0x04_common_concepts/variables/src/main.rs
